3a9528fc9eb45105a17ae8d3e3749a17c52d9502
rafael.fgoulart1 <rafael.fgoulart1@10.135.148.22>
2023-02-28 21:55:23 -0300

    Em andamento

--- src/main/java/goulart/rafael/aula28_02_2023/Array.java
+++ src/main/java/goulart/rafael/aula28_02_2023/Array.java
@@ -1,16 +1,18 @@
 package goulart.rafael.aula28_02_2023;
 
 public class Array {
-    private static Pessoa[] lista = new Pessoa[3];
+    private static Pessoa[] lista = new Pessoa[5];
     private static int indice = 0;
     
     public static void inserir(Pessoa p) {
+        p.setId(indice + 1);
         lista[indice++] = p;
     }
     
     public static void mostrar() {
         for(int i = 0; i < indice; i++) {
             System.out.println(lista[i]);
+            System.out.println("");
         }
     }
 }
--- src/main/java/goulart/rafael/aula28_02_2023/CadastroPessoas.java
+++ src/main/java/goulart/rafael/aula28_02_2023/CadastroPessoas.java
@@ -2,9 +2,18 @@ package goulart.rafael.aula28_02_2023;
 
 public class CadastroPessoas {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Rafael", 20, 1);
-        Pessoa p2 = new Pessoa("William", 36, 2);
+        Pessoa p1 = new Pessoa("Rafael", 20);
+        Pessoa p2 = new Pessoa("William", 36);
+        Pessoa p3 = new Pessoa("Marlon", 21);
+        Pessoa p4 = new Pessoa("Jéssica", 19);
+        Pessoa p5 = new Pessoa("Lara", 22);
+        
         Array.inserir(p1);
         Array.inserir(p2);
+        Array.inserir(p3);
+        Array.inserir(p4);
+        Array.inserir(p5);
+        
+        Array.mostrar();
     }
 }
